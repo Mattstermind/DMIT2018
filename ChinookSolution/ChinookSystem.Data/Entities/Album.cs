2b01e2dbde4d0c1fe724a89e63494ba56face7d2
Mattstermind <46451941+Mattstermind@users.noreply.github.com>
2020-01-09 09:06:46 -0700

    Created entities set up navigational properties

---
+++ ChinookSolution/ChinookSystem.Data/Entities/Album.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Data.Entities
+{
+    [Table("Albums")]
+    public class Album
+    {
+        private string _ReleaseLabel;
+
+        [Key]
+        public int AlbumId { get; set; }
+
+        [Required(ErrorMessage = "Album Title is required")]
+        [StringLength(160, ErrorMessage = "Artist Name can be no longer than 160 characters.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "ArtistId is required")]
+        public int ArtistId { get; set; }
+
+        [Required(ErrorMessage = "Release Year is required")]
+        public int ReleaseYear { get; set; }
+
+        [StringLength(50, ErrorMessage = "Artist Name can be no longer than 50 characters.")]
+        public string ReleaseLabel
+        {
+            get
+            {
+                return _ReleaseLabel;
+            }
+            set
+            {
+                //_ReleaseLabel = string.IsNullOrEmpty(value) ? null : value;
+                if (string.IsNullOrEmpty(value))
+                {
+                    _ReleaseLabel = null;
+                }
+                else
+                {
+                    _ReleaseLabel = value;
+                }
+            }
+        }
+
+        public virtual Artist Artist { get; set; }S
+
+    }
+}
---
+++ ChinookSolution/ChinookSystem.Data/Entities/Artist.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Data.Entities
+{
+    //Identify the sql entity (table) this class maps
+    [Table("Artists")]
+    public class Artist
+    {
+        //Fully Implemented Properties will be used for nullable strings.
+        private string _Name;
+
+        [Key]
+        public int ArtistId { get; set; }
+
+        [StringLength(120, ErrorMessage = "Artist Name can be no longer than 120 characters.")]
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                //_Name = string.IsNullOrEmpty(value) ? null : value;
+                if (string.IsNullOrEmpty(value))
+                {
+                    _Name = null;
+                }
+                else
+                {
+                    _Name = value;
+                }
+            }
+        }
+        /*Virtual navigational properties
+         * These properties do not contain data
+         * These properties form a virtual relationship
+         *  between the entity classes
+         *  you create the appropriate virtual properties such
+         *  as you are mapping the ERD relationship that exists in your database
+        */
+        public virtual ICollection<Album> Albums { get; set; }
+
+    }
+}
