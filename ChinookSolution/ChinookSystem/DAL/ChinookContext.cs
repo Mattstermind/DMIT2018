69d21cc64ed77ee70cb2cc5dbbb20f5f0cf76011
Mattstermind <46451941+Mattstermind@users.noreply.github.com>
2020-01-09 09:36:55 -0700

    Finish setting up the DAL and BLL classes

---
+++ ChinookSolution/ChinookSystem/DAL/ChinookContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+using ChinookSystem.Data.Entities;
+#endregion
+
+namespace ChinookSystem.DAL
+{
+    internal class ChinookContext:DbContext
+    {
+        //Reminder - Constructor is used to pass the connection string name
+        public ChinookContext() : base("ChinookDB")
+        {
+
+        }
+
+        //Set up the properties for the DbSet<>  used 
+        // to access the sql data
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums { get; set; }
+
+        //DTO and POCOs DO NOT get DbSet<> properties they dont represent entities only entities
+       // get DbSet<>
+    }
+}
