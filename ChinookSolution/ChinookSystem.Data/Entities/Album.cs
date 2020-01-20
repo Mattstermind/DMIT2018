db51abdb5cc56db48f04d70dbdadc06b9ba1e83d
Mattstermind <46451941+Mattstermind@users.noreply.github.com>
2020-01-20 13:37:38 -0700

    finally homework

--- ChinookSolution/ChinookSystem.Data/Entities/Album.cs
+++ ChinookSolution/ChinookSystem.Data/Entities/Album.cs
@@ -1,56 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-#region Additional Namespaces
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-#endregion
-
 namespace ChinookSystem.Data.Entities
 {
-    [Table("Albums")]
-    public class Album
-    {
-        private string _ReleaseLabel;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
 
-        [Key]
+    public partial class Album
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Album()
+        {
+            Tracks = new HashSet<Track>();
+        }
+
         public int AlbumId { get; set; }
 
-        [Required(ErrorMessage = "Album Title is required")]
-        [StringLength(160, ErrorMessage = "Artist Name can be no longer than 160 characters.")]
+        [Required(ErrorMessage ="Title is required")]
+        [StringLength(160, ErrorMessage = "Title is limited to 160 characters")]
         public string Title { get; set; }
 
-        [Required(ErrorMessage = "ArtistId is required")]
         public int ArtistId { get; set; }
 
-        [Required(ErrorMessage = "Release Year is required")]
         public int ReleaseYear { get; set; }
 
-        [StringLength(50, ErrorMessage = "Artist Name can be no longer than 50 characters.")]
-        public string ReleaseLabel
-        {
-            get
-            {
-                return _ReleaseLabel;
-            }
-            set
-            {
-                //_ReleaseLabel = string.IsNullOrEmpty(value) ? null : value;
-                if (string.IsNullOrEmpty(value))
-                {
-                    _ReleaseLabel = null;
-                }
-                else
-                {
-                    _ReleaseLabel = value;
-                }
-            }
-        }
+        [StringLength(50, ErrorMessage = "Release Label is limited to 50 characters")]
+        public string ReleaseLabel { get; set; }
 
         public virtual Artist Artist { get; set; }
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Track> Tracks { get; set; }
     }
 }
