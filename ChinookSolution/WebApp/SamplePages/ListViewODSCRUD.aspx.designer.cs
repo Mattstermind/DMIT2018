6a08a096a76e010c022c2b617310c5c106207def
Mattstermind <46451941+Mattstermind@users.noreply.github.com>
2020-01-22 14:49:48 -0700

    Update ListViewODSCRUD.aspx.designer.cs

--- ChinookSolution/WebApp/SamplePages/ListViewODSCRUD.aspx.designer.cs
+++ ChinookSolution/WebApp/SamplePages/ListViewODSCRUD.aspx.designer.cs
@@ -65,14 +65,5 @@ namespace WebApp.SamplePages {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ObjectDataSource ArtistODS;
-        
-        /// <summary>
-        /// ObjectDataSource3 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ObjectDataSource ObjectDataSource3;
     }
 }
