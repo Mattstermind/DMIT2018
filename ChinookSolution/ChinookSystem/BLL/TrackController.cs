b8de12d2ac4b1062aed639817f32295ece4a7b18
Mattstermind <46451941+Mattstermind@users.noreply.github.com>
2020-02-13 09:09:39 -0700

    kk

---
+++ ChinookSolution/ChinookSystem/BLL/TrackController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+#region Addition Namespaces
+using ChinookSystem.Data.Entities;
+using ChinookSystem.DAL;
+using System.ComponentModel;
+#endregion
+namespace ChinookSystem.BLL
+{   [DataObject]
+    public class TrackController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<Track> Get_TracksForPlaylistSelection(int id, string fetchby)
+        {
+            using (var context = new ChinookContext())
+            {
+                var results = from x in context.Tracks
+                              where x.AlbumId == id 
+                              select x;
+                return results.ToList();
+            }
+        }
+
+    }
+}
